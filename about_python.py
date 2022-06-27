e15dbf221a943504ebc58e1d8e6b630243795301
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-06-27 12:16:26 +0530

    Add files via upload

---
+++ about_python.py
@@ -0,0 +1,28 @@
+# to write python code ,following text editors / IDE 
+
+# 1> python 
+# 2> notepad 
+# 3> notepad++ 
+# 4> pycharm 
+# 5> spyder 
+# 6> subline 
+# 7> atom 
+# 8> thonny 
+# 9> brackets 
+# 10> microsoft visual studio code  
+
+# why we choose python means : it is a english language and it is a less line code .
+
+# python made by Guido Van Rassum , his's from 'dutch programmer' and his's DOB: 31-jan-1956 
+# python was developed at "centrum wiskunde & informatic " (CWI)
+
+# python got implemented / development in the year : dec-1989 
+
+# 'python version' :
+# * python got released in the year : 20-feb-1991 
+# * python2 got released in the year : 16-oct-2000 
+# * PSF: python software foundation on 6-march-2001 
+# python3 got released in the year  : 3-dec-2008
+# monty python's FLYING CIRCUS is the T.V serial the name python was fetched
+# it has 4seasons and was released in the year 1969 
+
